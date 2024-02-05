8bb7825e67954c5b09e1c5b40e0cf9f103d27d81
me <aszn.dev@gmail.com>
2024-02-05 11:22:57 +0600

    added task 10

--- task10/Program.cs
+++ task10/Program.cs
@@ -1,41 +1,45 @@
-﻿class Program()
-{
-    static void Main(){
-        var t = int.Parse(Console.ReadLine());
-        while(t-- > 0){
-            var N = int.Parse(Console.ReadLine());
-            var connection = new Dictionary<int, List<int>>();
-            var dict = new Dictionary<int, string>();
-            for(int i=0; i < N; i++){
-                var data = Console.ReadLine().Split(" ").ToList();
-                var id = int.Parse(data[0]);
-                var parentId = int.Parse(data[1]);
-                var text = string.Join(" ", data.Skip(2));
-                if(!connection.ContainsKey(parentId)){
-                    connection[parentId] = new List<int>();
-                }
-                connection[parentId].Add(id);
-                dict.Add(id, text);
-            }
-
-            void Process(int id, int deep){
-                if(dict.ContainsKey(id)){
-                    var tire = deep > 0 ? "--" : "";
-                    var palka = string.Join("  ", Enumerable.Repeat("|", deep));
-                    Console.WriteLine(palka + tire + dict[id]);
-                }
-                if(connection.ContainsKey(id)){
-                    Console.WriteLine(string.Join("  ", Enumerable.Repeat("|", deep + 1)));
-                    foreach(var item in connection[id].OrderBy(x => x)){
-                        Process(item, deep + 1);
-                    }
-                }
-                else{
-                    Console.WriteLine();
-                }
-            }
-
-            Process(-1, -1);
+﻿var t = int.Parse(Console.ReadLine());
+while(t-- > 0){
+    var N = int.Parse(Console.ReadLine());
+    var comments = new Dictionary<int, string>();
+    var isNodeLastOnLevel = new Dictionary<int, bool>();
+    var tree = new Dictionary<int, PriorityQueue<int, int>>();
+    for(int i=0; i < N; i++){
+        var input = Console.ReadLine().Split(" ").ToList();
+        var id = int.Parse(input[0]);
+        var parentId = int.Parse(input[1]);
+        var text = string.Join(" ", input.Skip(2));
+        comments.Add(id, text);
+        if(!tree.ContainsKey(parentId)){
+            tree.Add(parentId , new PriorityQueue<int, int>());
         }
+        tree[parentId].Enqueue(id, id);
     }
+    void PrintNode(int nodeId, int lvl){
+        if(lvl >= 0){
+            var line = new List<string>();
+            for(int i=1; i <= lvl; i++){
+                line.Add(i != lvl && isNodeLastOnLevel[i] ? "" : "|");
+            }
+            if(lvl > 0){
+                Console.WriteLine(string.Join("  ", line));
+                Console.Write(string.Join("  ", line));
+                Console.Write($"--");
+            }
+            Console.WriteLine($"{comments[nodeId]}");
+        }
+        if(tree.ContainsKey(nodeId)){
+            var cnt = tree[nodeId].Count;
+            while(cnt-- > 0){
+                var child = tree[nodeId].Dequeue();
+                if(!isNodeLastOnLevel.ContainsKey(lvl + 1)){
+                    isNodeLastOnLevel.Add(lvl + 1, cnt > 0);
+                }
+                isNodeLastOnLevel[lvl + 1] = cnt == 0;
+                PrintNode(child, lvl + 1);
+            }
+        }
+        if(lvl == 0) Console.WriteLine();
+    }
+    PrintNode(-1, -1);
 }
